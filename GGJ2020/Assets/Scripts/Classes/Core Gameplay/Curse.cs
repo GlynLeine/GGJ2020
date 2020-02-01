f327a044615b3eccee4e46c3e8581c6621d36651
Wiebe van Aken <wiebevaken@gmail.com>
2020-02-01 08:41:55 +0100

    Added Curse Repairable

---
+++ GGJ2020/Assets/Scripts/Classes/Core Gameplay/Curse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curse : ScriptableObject, ICurse, ISelectable
+{
+    [SerializeField]
+    private string curse;
+
+    public void Select()
+    {
+        
+    }
+
+    public bool TryRepair(ISpell spell)
+    {
+        return curse == spell.GetType();
+    }
+}
---
+++ GGJ2020/Assets/Scripts/Classes/Core Gameplay/Repairable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repairable : MonoBehaviour, IRepairable, ISelectable
+{
+    private ITransaction transaction;
+    private Action<IRepairable> callback;
+    private List<ICursable> cursables;
+    
+    public Repairable(ITransaction transaction)
+    {
+        this.transaction = transaction;
+    }
+
+    void OnClick()
+    {
+        if (callback != null)
+            callback.Invoke(this);
+    }
+
+    public void Select()
+    {
+        
+    }
+
+    public bool TryRepair(ISpell spell)
+    {
+        // TO DO
+        return false;
+    }
+
+    public void SubscribeToOnRepair(Action<IRepairable> callback)
+    {
+        this.callback = callback;
+    }
+
+    public ITransaction GetCost(ITransaction transaction)
+    {
+        
+    }
+}
