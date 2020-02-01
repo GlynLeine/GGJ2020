9a64618db7d82df2b4f2fd3fc561a0c4837dedd1
GlynLeine <445021@student.saxion.nl>
2020-02-01 20:17:51 +0100

    stuffs

---
+++ GGJ2020/Assets/Scripts/Misc/levitate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levitate : MonoBehaviour
+{
+    Vector2 position;
+
+    public float bobDistance = 0.5f;
+    public float bobSpeed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = position + new Vector2(0, Mathf.Sin(Time.time * bobSpeed) * bobDistance);
+    }
+}
