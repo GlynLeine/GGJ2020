d7414aeee54d7103da517b643a348c9545bef690
Wiebe van Aken <wiebevaken@gmail.com>
2020-02-01 01:31:39 +0100

    Implemented ICursable IDesk IRepairable IRepairableFactory ISpellBook

---
+++ GGJ2020/Assets/Scripts/Interfaces/Core Gameplay/ICursable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICursable
+{
+    public bool IsCursed();
+    public bool TryRepair(ISpell spell);
+}
