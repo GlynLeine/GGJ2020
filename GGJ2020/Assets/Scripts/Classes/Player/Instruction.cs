13d561a6999d30ed130c01cfbe0b2ab6b9fe919e
Wiebe van Aken <wiebevaken@gmail.com>
2020-02-02 03:52:38 +0100

    Implemented Spellbook

--- GGJ2020/Assets/Scripts/Classes/Player/Instruction.cs
+++ GGJ2020/Assets/Scripts/Classes/Player/Instruction.cs
@@ -8,6 +8,15 @@ public class Instruction
     public Instruction(string baseText, Spell spell, Repairable repairable, List<Tuple<Cursable, Curse>> curses)
     {
        // "SomeText <SpellWord> some more text <repairable> some more <curse0> blah <curse1> blah <curse2>"
+
+        if(curses.Count == 1)
+        {
+
+        }
+        else
+        {
+
+        }
     }
 
     public string GetText()
